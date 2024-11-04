0afb73be84997602b97954bb4b7b81e0ab3dcca0
Frankam487 <fkamgang06@gmail.com>
2024-11-04 12:53:33 -0800

    section offres added

--- src/components/Offres.jsx
+++ src/components/Offres.jsx
@@ -9,46 +9,43 @@ const Offres = () => {
         <div className="seo item">
           <h3>SEO</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur elit. Reprehenderit officia
-            autem libero!
+            Spécialiste SEO : Amélioration du référencement et du trafic web
           </p>
         </div>
         <div className="facebook item">
           <h3>Facebook Marketing</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit officia autem libero!
+            Concepteur de campagnes Facebook performantes pour augmenter la visibilité commerciale
           </p>
         </div>
         <div className="web item">
         <h1>&#128187;</h1>
           <h3>Youtube Marketing</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit officia autem libero!
+        Créateur de contenus youtube engageants pour audiences ciblées et croissance
           </p>
         </div>
         <div className="graphic item">
         <h1>&#9999;</h1>
           <h3>Content Writing</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit officia autem libero!
+            Rédacteur de contenus pertinents et engageants pour audience ciblées.
           </p>
         </div>
         <div className="youtube item">
           <h1>&lt;/&gt;</h1>
           <h3>Web Design</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit officia autem libero!
+            Concepteur d'expériences web intuitives et visuellement attrayantes.
           </p>
         </div>
         <div className="seo item">
           <h3>Graphic Design</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit officia autem libero!
+            Designeur graphique créatif pour logos, <br />
+            brochures et matériel marketing...
+            Créateur de visuels attrayants et d'identités visuelles cohérentes.
+            
           </p>
         </div>
       </div>
